d86614d3d3a89999df4da6815b05dc0c05eb74ce
Julien CHENE <jchene@e1r10p23.42.fr>
2022-03-31 17:59:18 +0200

    next try

--- philo/philo.h
+++ philo/philo.h
@@ -6,10 +6,17 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 16:14:03 by jchene            #+#    #+#             */
-/*   Updated: 2022/03/29 16:14:46 by jchene           ###   ########.fr       */
+/*   Updated: 2022/03/31 17:57:49 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <pthread.h>
+
 #endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,44 @@
 /*   By: jchene <jchene@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 16:14:52 by jchene            #+#    #+#             */
-/*   Updated: 2022/03/29 16:17:53 by jchene           ###   ########.fr       */
+/*   Updated: 2022/03/31 17:56:50 by jchene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../philo.h"
+
+unsigned int	ft_strlen
+
+int	check_charset(char *argv, const char *charset)
+{
+	unsigned int	i;
+
+
+	i = 0;
+}
+
+int	check_input(int argc, char **argv)
+{
+	unsigned int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (check_charset(argv[i], "0123456789") == -1)
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	pthread_t	**threads;
+	int			ret;
+
+	if (argc < 4 || argc > 5)
+		return (-1);
+	if (check_input(argc, argv) == -1)
+		return (-1);
+	return (0);
+}
